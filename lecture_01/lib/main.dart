ddeea96e0b67000c90317de7b7d898f2fced7e7f
Thitipong Tapianthong <thitipong.t@email.kmutnb.ac.th>
2024-12-18 12:09:42 +0700

    Refactor main.dart to implement a number increment/decrement feature and update widget structure

--- lecture_01/lib/main.dart
+++ lecture_01/lib/main.dart
@@ -1,65 +1,100 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
+// StatelessWidget
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: const Color.fromARGB(255, 255, 127, 167),
+        colorScheme: ColorScheme.fromSwatch().copyWith(
+          primary: const Color.fromARGB(255, 150, 236, 255),
         ),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Thitipong Tapianthong'),
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: const Color.fromARGB(255, 150, 236, 255),
+          title: const Text('Flutter Demo'),
+        ),
+        body: NumberWidget(),
+      ),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
+// StatefulWidget
+class NumberWidget extends StatefulWidget {
+  const NumberWidget({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<NumberWidget> createState() => _NumberWidgetState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _NumberWidgetState extends State<NumberWidget> {
+  int _number = 0;
+
+  void _increment() {
+    setState(() {
+      _number++;
+    });
+  }
+
+  void _decrement() {
+    setState(() {
+      _number--;
+    });
+  }
+
+  void _reset() {
+    setState(() {
+      _number = 0;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
-              "Thitipong Tapianthong",
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-            const SizedBox(height: 16),
-            ClipRRect(
-              borderRadius: BorderRadius.circular(24),
-              child: Image.network(
-                'https://scontent.fbkk21-1.fna.fbcdn.net/v/t39.30808-6/277530061_1651949845153059_1882076327746208940_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=QIs_YggMZREQ7kNvgE8PXD4&_nc_zt=23&_nc_ht=scontent.fbkk21-1.fna&_nc_gid=AglvLwHqMWU4KH_Lx8oTOqu&oh=00_AYD83UUSQzMvTcOUmI9MEdsBCGbOB3_9w5NO-SvI5pC1lw&oe=6758953B',
-                width: 200,
-                height: 200,
-              ),
-            ),
+            Text('$_number', style: Theme.of(context).textTheme.headlineLarge),
           ],
         ),
+      ),
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: <Widget>[
+          FloatingActionButton(
+            onPressed: _increment,
+            child: const Icon(Icons.add),
+          ),
+          const SizedBox(width: 8),
+          FloatingActionButton(
+            onPressed: _decrement,
+            child: const Icon(Icons.remove),
+          ),
+          const SizedBox(width: 8),
+          FloatingActionButton(
+            onPressed: _reset,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const <Widget>[
+                Icon(Icons.refresh),
+                SizedBox(height: 4),
+                Text('Reset'),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
